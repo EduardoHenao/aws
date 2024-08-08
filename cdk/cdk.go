74ec037dd92f81753382ca6ddcf2b2d4f173d2e2
Eduardo Henao <eduardo.henao@outlook.com>
2024-08-08 17:03:24 -0500

    add cdk files

---
+++ cdk/cdk.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awss3"
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+)
+
+type CdkStackProps struct {
+	awscdk.StackProps
+}
+
+func NewCdkStack(scope constructs.Construct, id string, props *CdkStackProps) awscdk.Stack {
+	var sprops awscdk.StackProps
+	if props != nil {
+		sprops = props.StackProps
+	}
+	stack := awscdk.NewStack(scope, &id, &sprops)
+
+	awss3.NewBucket(stack, jsii.String("hello-world-789"), &awss3.BucketProps{
+		Versioned: jsii.Bool(true), // Example property, makes the bucket versioned
+	})
+
+	return stack
+}
+
+func main() {
+	defer jsii.Close()
+
+	app := awscdk.NewApp(nil)
+
+	NewCdkStack(app, "CdkStack-ed", &CdkStackProps{
+		awscdk.StackProps{
+			Env: env(),
+		},
+	})
+
+	app.Synth(nil)
+}
+
+func env() *awscdk.Environment {
+	return &awscdk.Environment{
+		Account: jsii.String("776142153476"),
+		Region:  jsii.String("us-east-1"),
+	}
+}
